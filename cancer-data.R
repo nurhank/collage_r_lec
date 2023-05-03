58ca0790e565582f7f9cee95288209362dae4d1d
nurhank <nurhan.astro@gmail.com>
2023-05-03 17:21:16 +0400

    Update cancer-data.R

---
+++ cancer-data.R
@@ -0,0 +1,20 @@
+df <- read.csv("Desktop/Cancer_Data.csv")
+vec1 <- df$radius_mean
+vec1
+
+max(vec1)
+min(vec1)
+mean(vec1)
+
+# if (85 < my.age < 100) {
+#   print("You passed this course with AA")
+# } else if (my.age >= 75 && my.age <= 85)
+# {
+#   print("You passed this course with BA")
+# } else
+# {
+#   print("S/he failed the course under other conditions")
+# }
+
+
+
